17aa25d292914a0a882a3f74bd6d2d68c7bd8a1f
Pjai1 <ulti40@hotmail.com>
2017-07-31 15:09:06 +0200

    Component/Module Lazy Loading

--- New_Technology/ng2-fundamentals/app/app.module.ts
+++ New_Technology/ng2-fundamentals/app/app.module.ts
@@ -13,6 +13,7 @@ import { Error404Component } from './errors/404.component'
 import { EventService } from './events/shared/events.service'
 import { EventRouteActivator } from './events/event-details/event-route-activator.service'
 import { ToastrService } from './common/toastr.service'
+import { EventListResolver } from './events/events-list-resolver.service'
 
 import { appRoutes } from './routes'
 
@@ -34,6 +35,7 @@ import { appRoutes } from './routes'
         EventService,
         ToastrService, 
         EventRouteActivator,
+        EventListResolver,
         { 
             provide: 'canDeactivateCreateEvent', 
             useValue: checkDirtyState
--- New_Technology/ng2-fundamentals/app/events/events-list.component.ts
+++ New_Technology/ng2-fundamentals/app/events/events-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 import { EventService } from './shared/events.service'
 import { ToastrService } from '../common/toastr.service'
+import { ActivatedRoute } from '@angular/router'
 
 @Component({
     template: `
@@ -17,14 +18,14 @@ import { ToastrService } from '../common/toastr.service'
 })
 
 export class EventsListComponent implements OnInit {
-    events:any[]
+    events:any
 
-    constructor(private eventService:EventService, private toastrService:ToastrService) {
+    constructor(private eventService:EventService, private route:ActivatedRoute, private toastrService:ToastrService) {
 
     }
 
     ngOnInit() {
-        this.events = this.eventService.getEvents();
+        this.events = this.route.snapshot.data['events']
     }
 
     handleThumbnailClick(eventName) {
