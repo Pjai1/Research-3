066642bee6042e843b279400b572b8910e78c811
Pjai1 <ulti40@hotmail.com>
2017-07-31 14:22:27 +0200

    Route Activate/Deactivate

--- New_Technology/ng2-fundamentals/app/app.module.ts
+++ New_Technology/ng2-fundamentals/app/app.module.ts
@@ -8,8 +8,10 @@ import { EventThumbnailComponent } from './events/event-thumbnail.component'
 import { NavBarComponent } from './nav/navbar.component'
 import { EventDetailsComponent } from './events/event-details/event-details.component'
 import { CreateEventComponent } from './events/create-event.component'
+import { Error404Component } from './errors/404.component'
 
 import { EventService } from './events/shared/events.service'
+import { EventRouteActivator } from './events/event-details/event-route-activator.service'
 import { ToastrService } from './common/toastr.service'
 
 import { appRoutes } from './routes'
@@ -25,13 +27,25 @@ import { appRoutes } from './routes'
         EventThumbnailComponent,
         NavBarComponent,
         EventDetailsComponent,
-        CreateEventComponent
+        CreateEventComponent, 
+        Error404Component
     ],
     providers: [
         EventService,
-        ToastrService
+        ToastrService, 
+        EventRouteActivator,
+        { 
+            provide: 'canDeactivateCreateEvent', 
+            useValue: checkDirtyState
+        }
     ],
     bootstrap: [EventsAppComponent]
 })
 
 export class AppModule {}
+
+function checkDirtyState(component:CreateEventComponent) {
+    if(component.isDirty)
+        return window.confirm('You have not saved this event yet, still want to continue?')
+    return true
+}
