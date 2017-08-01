c83faebdb355e8c9f3d11e4d89b989273d833308
Pjai1 <ulti40@hotmail.com>
2017-08-01 13:23:17 +0200

    Inter-Component Comms

--- New_Technology/ng2-fundamentals/app/events/event-details/event-details.component.ts
+++ New_Technology/ng2-fundamentals/app/events/event-details/event-details.component.ts
@@ -1,22 +1,40 @@
 import { Component, OnInit } from '@angular/core'
 import { EventService } from '../shared/events.service'
-import { IEvent } from '../shared/index'
+import { IEvent, ISession } from '../shared/index'
 
 @Component({
     templateUrl: 'app/events/event-details/event-details.component.html',
     styles: [`
         .container { padding-right: 20px, padding-left: 20px; }
         .event-image { height: 100px; }
+        a { cursor: pointer }
     `]
 })
 
 export class EventDetailsComponent implements OnInit {
     event:IEvent
+    addMode:boolean
 
     constructor(private eventService:EventService) {
 
     }
     ngOnInit() {
         this.event = this.eventService.getEvent(1);
+    }
+
+    addSession() {
+        this.addMode = true
+    }
+
+    saveNewSession(session:ISession) {
+        const nextId = Math.max.apply(null, this.event.sessions.map(s => s.id))
+        session.id = nextId + 1
+        this.event.sessions.push(session)
+        this.eventService.updateEvent(this.event)
+        this.addMode = false
+    }
+
+    cancelAddSession() {
+        this.addMode = false
     }
 }
