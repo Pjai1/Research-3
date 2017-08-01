dd6d48baf1726b03fdb1ec61bcd005edfdc05f51
Pjai1 <ulti40@hotmail.com>
2017-08-01 02:09:17 +0200

    More Custom Validation

--- New_Technology/ng2-fundamentals/app/events/event-details/create-session.component.ts
+++ New_Technology/ng2-fundamentals/app/events/event-details/create-session.component.ts
@@ -1,9 +1,17 @@
 import { Component, OnInit } from '@angular/core'
 import { FormControl, FormGroup, Validators } from '@angular/forms'
-import { ISession } from '../shared/index'
+import { ISession, restrictedWords } from '../shared/index'
 
 @Component({
-    templateUrl: 'app/events/event-details/create-session.component.html'
+    templateUrl: 'app/events/event-details/create-session.component.html',
+    styles: [`
+        em { float:right; color:#E05C65; padding-left: 10px; }
+        .error input, .error select, .error textarea { background-color:#E3C3C5 }
+        .error ::-webkit-input-placeholder { color:#999; }
+        .error ::-moz-placeholder { color:#999; }
+        .error :-moz-placeholder { color:#999; }
+        .error :ms-input-placeholder { color:#999; }
+    `]
 })
 
 export class CreateSessionComponent implements OnInit{
@@ -19,7 +27,7 @@ export class CreateSessionComponent implements OnInit{
         this.presenter = new FormControl('', Validators.required)
         this.duration = new FormControl('', Validators.required)
         this.level = new FormControl('', Validators.required)
-        this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400)])
+        this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400), restrictedWords(['foo', 'bar'])])
         this.newSessionForm = new FormGroup({
             name: this.name,
             presenter: this.presenter,
@@ -29,6 +37,16 @@ export class CreateSessionComponent implements OnInit{
         })
     }
 
+    private restrictedWords(words) {
+        return (control: FormControl): {[key:string]: any} =>
+        {
+            if(!words) return null
+
+            var invalidWords = words.map(w => control.value.includes(w) ? w : null).filter(w => w != null)
+            return control.value.includes('foo') ? {'restrictedWords': invalidWords.join(', ')} : null
+        }
+    }
+    
     saveSession(formValues) {
         let session:ISession = {
             id: undefined,
