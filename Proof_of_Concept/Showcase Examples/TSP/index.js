2a9b819c61c21d009fc634b2f2f6f72cca29b5f9
Pjai1 <ulti40@hotmail.com>
2017-08-11 14:46:18 +0200

    Rewriting TSP drawing logic

--- Proof_of_Concept/Showcase Examples/TSP/index.js
+++ Proof_of_Concept/Showcase Examples/TSP/index.js
@@ -1,74 +1,70 @@
-var cities = [];
+var cities = []; //vector of cities
 const totalCities = 6;
 
-var recordDistance;
+var population = [];
+var fitness = [];
+
+var recordDistance = Infinity;
 var bestEver;
 
 //draw canvas
 var setup = () => {
-  createCanvas(1024, 768);
+  createCanvas(800, 600);
+  let order = []; //how to go through all cities
 
   for (let i = 0; i < totalCities; i++) {
     let v = createVector(random(width), random(height));
     cities[i] = v;
+    order[i] = i;
   }
 
-  let d = calcDist(cities);
-  recordDistance = d;
-  bestEver = cities.slice();
+  for (let i = 0; i < 100; i++) {
+    population[i] = shuffle(order); //p5 shuffle fn
+  }
+  console.log(population)
 
+  for (let i = 0; i < population.length; i++) {
+    let d = calcDist(cities, population[i]);
+    if (d < recordDistance) {
+      recordDistance = d;
+      bestEver = population[i];
+    }
+    fitness[i] = d;
+  }
 }
 
 //draw all cities and routes between them
 var draw = () => {
   background(0);
-  fill(255);
-  for (let i = 0; i < cities.length; i++) {
-    ellipse(cities[i].x, cities[i].y, 8, 8);
-  }
-
-  stroke(255);
-  strokeWeight(1);
-  noFill();
-  beginShape();
-  for (let i = 0; i < cities.length; i++) {
-    vertex(cities[i].x, cities[i].y);
-  }
-  endShape();
 
   stroke(255, 0, 255);
   strokeWeight(4);
   noFill();
   beginShape();
-  for (let i = 0; i < cities.length; i++) {
-    vertex(bestEver[i].x, bestEver[i].y);
+  for (let i = 0; i < bestEver.length; i++) {
+    let j = bestEver[i];
+    vertex(cities[j].x, cities[j].y);
+    ellipse(cities[j].x, cities[j].y, 16, 16);
   }
   endShape();
-
-  let i = floor(random(cities.length));
-  let j = floor(random(cities.length));
-  swap(cities, i, j);
-
-  let d = calcDist(cities);
-
-  if (d < recordDistance) {
-    recordDistance = d;
-    bestEver = cities.slice();
-  }
 }
 
-//try different connection from current node
-var swap = (a, i, j) => {
-  let temp = a[i];
-  a[i] = a[j];
-  a[j] = temp;
-}
+//try different connection from current node aka shuffling
+// var swap = (a, i, j) => {
+//   let temp = a[i];
+//   a[i] = a[j];
+//   a[j] = temp;
+// }
 
 //calculate distance between two locations
-var calcDist = (points) => {
+var calcDist = (points, order) => {
   let sum = 0;
-  for (let i = 0; i < points.length - 1; i++) {
-    let d = dist(points[i].x, points[i].y, points[i + 1].x, points[i + 1].y);
+  for (let i = 0; i < order.length - 1; i++) {
+    let cityAIndex = order[i];
+    let cityA = points[cityAIndex];
+    let cityBIndex = order[i+1];
+    let cityB = points[cityBIndex];
+    let d = dist(cityA.x, cityA.y, cityB.x, cityB.y);
     sum += d;
   }
   return sum;
